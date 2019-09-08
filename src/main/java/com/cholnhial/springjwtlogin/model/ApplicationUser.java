3116df9003df197e56e103f9fde0562b3e937138
Chol Nhial <chol.nhial.chol@gmail.com>
2019-09-08 15:44:06 +1000

    Make ApplicationUser an entity and add @Id annotation

--- src/main/java/com/cholnhial/springjwtlogin/model/ApplicationUser.java
+++ src/main/java/com/cholnhial/springjwtlogin/model/ApplicationUser.java
@@ -4,13 +4,17 @@ package com.cholnhial.springjwtlogin.model;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
+@Entity
 public class ApplicationUser {
 
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
     private Long id;
 
     @EqualsAndHashCode.Include
