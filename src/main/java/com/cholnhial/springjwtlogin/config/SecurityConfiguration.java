b2a40f1223e093118eff23e743b54de1564c44e6
Chol Nhial <chol.nhial.chol@gmail.com>
2019-09-08 15:44:54 +1000

    Fix password encoder bean wiring

--- src/main/java/com/cholnhial/springjwtlogin/config/SecurityConfiguration.java
+++ src/main/java/com/cholnhial/springjwtlogin/config/SecurityConfiguration.java
@@ -1,7 +1,11 @@
 package com.cholnhial.springjwtlogin.config;
 
+import com.cholnhial.springjwtlogin.security.AuthenticationFilter;
+import com.cholnhial.springjwtlogin.security.AuthorizationFilter;
+import com.cholnhial.springjwtlogin.service.ApplicationUserDetailsService;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,6 +13,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -19,12 +24,12 @@ import static com.cholnhial.springjwtlogin.constant.SecurityConstants.SIGN_UP_UR
 @AllArgsConstructor
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    private ApplicationUserService userDetailsService;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final ApplicationUserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
     }
 
     @Override
--- src/main/java/com/cholnhial/springjwtlogin/constant/SecurityConstants.java
+++ src/main/java/com/cholnhial/springjwtlogin/constant/SecurityConstants.java
@@ -2,7 +2,7 @@ package com.cholnhial.springjwtlogin.constant;
 
 public class SecurityConstants {
     public static final String SIGN_UP_URL = "/users/record";
-    public static final String KEY = "(G+KbPeShVmYq3t6w9y$B&E)H@McQfTjWnZr4u7x!A%C*F-JaNdRgUkXp2s5v8y/";
+    public static final String KEY = "q3t6w9z$C&F)J@NcQfTjWnZr4u7x!A%D*G-KaPdSgUkXp2s5v8y/B?E(H+MbQeTh";
     public static final String HEADER_NAME = "Authorization";
     public static final Long EXPIRATION_TIME = 1000L*60*30;
 }
