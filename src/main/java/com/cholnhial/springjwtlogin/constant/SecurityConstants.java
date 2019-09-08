327d47faf2e02a542f6e4deb2edc83e500ada64a
Chol Nhial <chol.nhial.chol@gmail.com>
2019-09-08 13:31:10 +1000

    Add security constants and Security config

---
+++ src/main/java/com/cholnhial/springjwtlogin/constant/SecurityConstants.java
@@ -0,0 +1,8 @@
+package com.cholnhial.springjwtlogin.constant;
+
+public class SecurityConstants {
+    public static final String SIGN_UP_URL = "/users/record";
+    public static final String KEY = "(G+KbPeShVmYq3t6w9y$B&E)H@McQfTjWnZr4u7x!A%C*F-JaNdRgUkXp2s5v8y/";
+    public static final String HEADER_NAME = "Authorization";
+    public static final Long EXPIRATION_TIME = 1000L*60*30;
+}
